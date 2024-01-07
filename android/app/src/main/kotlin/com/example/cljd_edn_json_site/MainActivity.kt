dc2c7371995ab7b497b508a68900635bb92e9106
roboli <robertooliveros@mac.com>
2024-01-07 09:12:50 -0600

    Import files

---
+++ android/app/src/main/kotlin/com/example/cljd_edn_json_site/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cljd_edn_json_site
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+export "cljd-out/edn-json/main.dart" show main;
